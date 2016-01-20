127a2255b69c8e373e79c557cf5beb8b8f6b05f5
hinagishi <archangel.ratziel@gmail.com>
2016-01-20 18:29:47 +0900

    add -u option

--- main.go
+++ main.go
@@ -23,32 +23,67 @@ func setBody() string {
 	return body
 }
 
-func setMeta() string {
+func setMeta(opt Options) string {
+	fmt.Println(opt)
 	meta := "\\author{"
-	us, err := user.Current()
-	if err != nil {
-		meta += "unknown}"
+
+	if opt.Author != "" {
+		fmt.Println(opt.Author)
+		meta += opt.Author + "}"
 	} else {
-		meta += us.Username + "}"
+		fmt.Println(opt.Author)
+		us, err := user.Current()
+		if err != nil {
+			meta += "unknown}"
+		} else {
+			meta += us.Username + "}"
+		}
 	}
 	meta += "\n\\title{no-title}\n"
 	return meta
+}
 
+func usage() {
+	fmt.Println("Usage texgen [options] filename")
+}
+
+type Options struct {
+	Filename string
+	Author   string
+	Title    string
 }
 
 func main() {
+	var opt Options
 	if len(os.Args) < 2 {
-		fmt.Println("Usage texgen [options] filename")
+		usage()
 		return
 	}
-	wf, err := os.OpenFile(os.Args[1], os.O_RDWR|os.O_CREATE, 0644)
+	for i := 1; i < len(os.Args); i++ {
+		if os.Args[i] == "-u" {
+			if i+1 >= len(os.Args) {
+				usage()
+				return
+			}
+			opt.Author = os.Args[i+1]
+			i++
+		} else {
+			opt.Filename = os.Args[i]
+		}
+	}
+	fmt.Println(opt)
+	if opt.Filename == "" {
+		usage()
+		return
+	}
+	wf, err := os.OpenFile(opt.Filename, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		return
 	}
 	defer wf.Close()
 	writer := bufio.NewWriter(wf)
 	writer.Write([]byte(setHeader()))
-	writer.Write([]byte(setMeta()))
+	writer.Write([]byte(setMeta(opt)))
 	writer.Write([]byte(setPackages()))
 	writer.Write([]byte(setBody()))
 	writer.Flush()
